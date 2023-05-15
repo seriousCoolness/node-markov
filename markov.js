ee1b86971aa279b2d67bff7021d315e2ea2cf69e
robert <wyattrobert42@gmail.com>
2023-05-14 21:01:52 -0400

    finshed exercise

--- markov.js
+++ markov.js
@@ -1,6 +1,5 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
 
   /** build markov machine; read in text.*/
@@ -17,13 +16,37 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    
+    let markov_chain = new Map();
+    
+    for (let i =0; i < this.words.length; i++)
+    {
+      let current_word = this.words[i];
+      if(markov_chain.has(current_word))
+      {
+        markov_chain.get(current_word).push(this.words[i+1]);
+      }
+      else
+        markov_chain.set(current_word, new Array().push(this.words[i+1]))
+    }
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let key = MarkovMachine.choice(Array.from(this.chains.keys()));
+    let out = [];
+
+    while (out.length < numWords && key !== null) {
+      out.push(key);
+      key = MarkovMachine.choice(this.chains.get(key));
+    }
+
+    return out.join(" ");
   }
 }
+
+module.exports = {
+  MarkovMachine,
+};
